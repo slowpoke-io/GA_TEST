3e5b8b3651a7416dd549760633b719c102bfb7df
slowpoke-io <b094020008@gmail.com>
2025-11-13 02:41:18 +0800

    fr VITE_API_BASE

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Page GA {import.meta.env.CUSTOM_DATA || "PR"}</h1>
+      <h1>Vite + React Page GA {import.meta.env.VITE_API_BASE || "PR"}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
