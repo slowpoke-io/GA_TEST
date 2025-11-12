2bc12baae612c24511bb744666e7339b6ab65f6a
slowpoke-io <b094020008@gmail.com>
2025-11-13 02:03:06 +0800

    pr preview

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Page GA {import.meta.env.CUSTOM_DATA || ""}</h1>
+      <h1>Vite + React Page GA {import.meta.env.CUSTOM_DATA || "PR"}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
